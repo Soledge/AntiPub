64b53b0d29fd8ee7ad0bebc11abdf53613e1453c
Wolfy Wolfington <wolfy9247@gmail.com>
2013-03-16 16:54:15 -0500

    Fixed and updated switch cases in Format.java.

--- src/main/java/com/wolfy9247/AntiPub/Format.java
+++ src/main/java/com/wolfy9247/AntiPub/Format.java
@@ -3,29 +3,31 @@ package main.java.com.wolfy9247.AntiPub;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class Format extends AntiPub {
+public class Format {
 	
 	private final String[] tokens = {"<user>", "<message>", "<ip>", "<hostname>", "<port>"};
 	private String message;
 	private Player player;
+	private String playerMessage;
 	
 	public Format(AsyncPlayerChatEvent e) {
 		player = e.getPlayer();
+		playerMessage = e.getMessage();
 	}
 	
-	public String formatMessage(String m) {
-		message = m;
+	public String formatMessage(String str) {
+		message = str;
 		for(int i = 0; i < tokens.length; i++) {
 			switch(i) {
-				case 1: message.replace(tokens[i], player.getName());
+				case 0: message = message.replace(tokens[i], player.getName());
 						break;
-				case 2: message.replace(tokens[i], message);
+				case 1: message = message.replace(tokens[i], "\"" + playerMessage + "\"");
 						break;
-				case 3: message.replace(tokens[i], player.getAddress().toString());
+				case 2: message = message.replace(tokens[i], player.getAddress().getAddress().getHostAddress());
 						break;
-				case 4: message.replace(tokens[i], player.getAddress().getHostName());
+				case 3: message = message.replace(tokens[i], player.getAddress().getAddress().getCanonicalHostName());
 						break;
-				case 5: message.replace(tokens[i], new Integer(player.getAddress().getPort()).toString());
+				case 4: message = message.replace(tokens[i], new Integer(player.getAddress().getPort()).toString());
 						break;
 			}
 		}
