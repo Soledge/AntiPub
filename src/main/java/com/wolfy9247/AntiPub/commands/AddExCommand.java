b85e04a3baefa8ec8f73b3eb10da4f6f21745176
Wolfy Wolfington <wolfy9247@gmail.com>
2013-03-16 16:28:34 -0500

    Added placeholder code for add/remove exception commands.

--- src/main/java/com/wolfy9247/AntiPub/commands/AddExCommand.java
+++ src/main/java/com/wolfy9247/AntiPub/commands/AddExCommand.java
@@ -1,54 +1,31 @@
-/**
- * 
- */
 package main.java.com.wolfy9247.AntiPub.commands;
-
 
 import main.java.com.wolfy9247.AntiPub.AntiPub;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.ChatColor;
 
 
-/**
- * @author Wolfy
- *
- */
 public class AddExCommand implements APCommand {
 
-	/* (non-Javadoc)
-	 * @see main.java.com.wolfy9247.AntiPub.commands.APCommand#getPermissionNode()
-	 */
 	@Override
 	public String getPermissionNode() {
-		// TODO Auto-generated method stub
-		return null;
+		return "ap.addx";
 	}
 
-	/* (non-Javadoc)
-	 * @see main.java.com.wolfy9247.AntiPub.commands.APCommand#getNames()
-	 */
 	@Override
 	public String[] getNames() {
-		// TODO Auto-generated method stub
-		return null;
+		return new String[]{"addx"};
 	}
 
-	/* (non-Javadoc)
-	 * @see main.java.com.wolfy9247.AntiPub.commands.APCommand#getSyntax()
-	 */
 	@Override
 	public String getSyntax() {
-		// TODO Auto-generated method stub
-		return null;
+		return "addx <exception>";
 	}
 
-	/* (non-Javadoc)
-	 * @see main.java.com.wolfy9247.AntiPub.commands.APCommand#execute(main.java.com.wolfy9247.AntiPub.AntiPub, org.bukkit.command.CommandSender, java.lang.String[])
-	 */
 	@Override
 	public void execute(AntiPub plugin, CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-
+		sender.sendMessage(ChatColor.RED + "[AntiPub] This feature has not yet been implemented in this version of AntiPub.");
 	}
 
 }
--- src/main/java/com/wolfy9247/AntiPub/commands/RemoveExCommand.java
+++ src/main/java/com/wolfy9247/AntiPub/commands/RemoveExCommand.java
@@ -1,35 +1,30 @@
 package main.java.com.wolfy9247.AntiPub.commands;
 
-
 import main.java.com.wolfy9247.AntiPub.AntiPub;
 
 import org.bukkit.command.CommandSender;
-
+import org.bukkit.ChatColor;
 
 public class RemoveExCommand implements APCommand {
 
 	@Override
 	public String getPermissionNode() {
-		// TODO Auto-generated method stub
-		return null;
+		return "ap.remx";
 	}
 
 	@Override
 	public String[] getNames() {
-		// TODO Auto-generated method stub
-		return null;
+		return new String[]{"remx"};
 	}
 
 	@Override
 	public String getSyntax() {
-		// TODO Auto-generated method stub
-		return null;
+		return "remx <exception>";
 	}
 
 	@Override
 	public void execute(AntiPub plugin, CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-
+		sender.sendMessage(ChatColor.RED + "[AntiPub] This feature has not yet been implemented in this version of AntiPub.");
 	}
 
 }
